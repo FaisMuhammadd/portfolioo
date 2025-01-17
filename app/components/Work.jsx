c811a6f053362dca9e20b507b6db9f11bb4da320
pandawaa <pallakangoding@gmail.com>
2025-01-18 05:52:20 +0800

    modified Work

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -6,17 +6,17 @@ const Work = ({isDarkMode}) => {
   return (
     <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
       <h4 className='text-center mb-2 text-lg font-Ovo'>
-         My Portfolio
+         My Projects
       </h4>
       <h2 className="text-center text-5xl font-Ovo">
          My latest work
       </h2>
       <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
-         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti amet commodi facere voluptatibus ea. Ad nam debitis tempore nisi laboriosam!
+         I've embarked on several projects throughout the years, but these are the ones I hold closest to my heart. Many of them are open-source
       </p>
 
       <div className='grid grid-cols-auto my-10 gap-5 dark:text-black'>
-         {workData.map((project, index) => (
+         {workData.map((project, index, description) => (
             <div key={index} style={{backgroundImage: `url(${project.bgImage})`}} className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'>
                <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
                   <div>
