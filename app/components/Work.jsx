586a2116770c7843576f5b914514fa47842fa514
Fais <pallakangoding@.com>
2025-01-19 09:51:30 +0800

    modified Work

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -2,7 +2,7 @@ import { assets, workData } from '@/assets/assets';
 import Image from 'next/image';
 import React from 'react'
 
-const Work = ({isDarkMode}) => {
+const Work = () => {
   return (
     <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
       <h4 className='text-center mb-2 text-lg font-Ovo'>
@@ -16,7 +16,7 @@ const Work = ({isDarkMode}) => {
       </p>
 
       <div className='grid grid-cols-auto my-10 gap-5 dark:text-black'>
-         {workData.map((project, index, description) => (
+         {workData.map((project, index, link) => (
             <div key={index} style={{backgroundImage: `url(${project.bgImage})`}} className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'>
                <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
                   <div>
@@ -24,16 +24,15 @@ const Work = ({isDarkMode}) => {
                      <p className='text-sm text-gray-700'>{project.description}</p>
                   </div>
                   <div className='border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition'>
-                     <Image src={assets.send_icon} alt='send' className='w-5'/>
+                     <a href={project.link}>
+                        <Image src={assets.send_icon} alt='send' className='w-5'/>
+                     </a>
                   </div>
                </div>
             </div>
          ))}
       </div>
 
-      <a href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover'>
-         Show more <Image src={isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='right-arrow' className='w-4'/>
-      </a>
     </div>
   )
 }
